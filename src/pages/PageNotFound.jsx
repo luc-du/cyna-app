553bc1e13b779f4e412ba28579a0f5e0a464de11
GDevWeb <zodiaquestar@gmail.com>
2025-02-28 17:58:37 +0100

    Correction du message d'erreur sur la page 404 et remplacement du lien par un composant Link pour une navigation améliorée.

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 const PageNotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
@@ -6,14 +7,14 @@ const PageNotFound = () => {
       </h1>
       <h2 className="text-4xl font-semibold mt-4">Page Not Found</h2>
       <p className="text-lg text-gray-600 mt-2">
-        Oops! La page demandée n'exite pas.
+        Oops! La page demandée n'existe pas.
       </p>
-      <a
-        href="/"
+      <Link
+        to="/"
         className="mt-6 px-6 py-3 bg-purple-600 text-white rounded-full shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-xl"
       >
         Retour à la page d'accueil
-      </a>
+      </Link>
     </div>
   );
 };
