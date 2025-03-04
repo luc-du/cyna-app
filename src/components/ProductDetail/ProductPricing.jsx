99e19ffcfa0a444a6e6c1ab13430d16d8defa682
GDevWeb <zodiaquestar@gmail.com>
2025-03-04 18:35:43 +0100

    Tri des options de tarification dans ProductPricing pour afficher les services disponibles en premier

--- src/components/ProductDetail/ProductPricing.jsx
+++ src/components/ProductDetail/ProductPricing.jsx
@@ -2,10 +2,17 @@ import { MOCK_PricingOptions } from "../../mock/MOCKS_DATA.jsx";
 import CardProductPricing from "./CardProductPricing.jsx";
 
 const ProductPricing = () => {
+  const pricingOptions = MOCK_PricingOptions;
   // 1.State :
   // 2.Functions:
+  const sortedPricingOptions = pricingOptions.sort((a, b) => {
+    if (a.available === b.available) {
+      return a.available - b.available;
+    } else {
+      return b.available - a.available;
+    }
+  });
   // 3. Others
-  const pricingOptions = MOCK_PricingOptions;
 
   // 4.Render
   return (
@@ -14,7 +21,7 @@ const ProductPricing = () => {
         Tarifs du service
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {pricingOptions.map((option, index) => (
+        {sortedPricingOptions.map((option, index) => (
           <CardProductPricing option={option} key={index} />
         ))}
       </div>
