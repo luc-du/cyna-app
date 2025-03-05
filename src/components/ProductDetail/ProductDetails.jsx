ce9efd2ee39ed1e9011813065aa2f8fd9d8b49b4
GDevWeb <zodiaquestar@gmail.com>
2025-03-05 12:12:50 +0100

    Refactorisation de ProductCTA pour intégrer la gestion du panier et amélioration de l'affichage en cas de produit non trouvé

--- src/components/ProductDetail/ProductDetails.jsx
+++ src/components/ProductDetail/ProductDetails.jsx
@@ -2,8 +2,8 @@ import { useParams } from "react-router-dom";
 import { indexImages } from "../../assets/indexImages";
 import useFindById from "../../hooks/useFindById";
 import { MOCK_Services } from "../../mock/MOCKS_DATA";
-import AddToCartButton from "../ui/buttons/AddToCartButton";
 import ProductCarousel from "./ProductCarousel";
+import ProductCTA from "./ProductCTA";
 import ProductInfo from "./ProductInfo";
 import ProductPricing from "./ProductPricing";
 import ProductSpecs from "./ProductSpecs";
@@ -18,7 +18,11 @@ const ProductDetails = () => {
   let content;
 
   if (!service) {
-    content = <h2>Produit non trouvé ⁉️</h2>;
+    content = (
+      <div className="max-w-6xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+        <h2 className="font-2xl text-center">Service non trouvé ⁉️</h2>
+      </div>
+    );
   } else {
     content = (
       <div className="max-w-6xl mx-auto p-6 bg-white rounded-lg shadow-lg">
@@ -29,7 +33,8 @@ const ProductDetails = () => {
         {/* 3. Carac. techniques 🕹️ */}
         <ProductSpecs product={service} />
         {/* 4. CTA ajout au panier 🛒 */}
-        <AddToCartButton product={service} />
+        <ProductCTA product={service} />
+
         {/* 5. Prix des services 💰 */}
         <ProductPricing product={service} />
         {/* 6. Services similaires 🔄 */}
