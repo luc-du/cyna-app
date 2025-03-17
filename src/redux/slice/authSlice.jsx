193bb0d95beb96ed161fa980903b70c8b3936c44
GDevWeb <zodiaquestar@gmail.com>
2025-03-17 21:19:58 +0100

    mise en place de L'api auth -- fuction nsignup & signin & validate token; utilisation de axios

--- src/redux/slice/authSlice.jsx
+++ src/redux/slice/authSlice.jsx
@@ -6,14 +6,53 @@ const API_BASE_URL = "http://localhost:8080/api/v1/auth";
 /* Signup de user */
 export const registerUser = createAsyncThunk(
   "auth/register",
-  async (userData, { isRejectedWithValue }) => {
+  async (userData, { rejectWithValue }) => {
     try {
       const response = await axios.post(`${API_BASE_URL}/signup`, userData);
       return response.data;
     } catch (error) {
-      return isRejectedWithValue(
+      return rejectWithValue(
         error.response?.data || "Erreur lors de l'inscription"
       );
+    }
+  }
+);
+
+/* Login user */
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/signin`, credentials);
+      localStorage.setItem("token", response.data.token);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "Pair login incorrect!");
+    }
+  }
+);
+
+/* Check du token */
+export const validateToken = createAsyncThunk(
+  "auth/validateToken",
+  async (_, { rejectWithValue }) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        throw new Error("Token non disponible");
+      }
+
+      const response = await axios.post(
+        `${API_BASE_URL}/validate`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "Token invalide");
     }
   }
 );
