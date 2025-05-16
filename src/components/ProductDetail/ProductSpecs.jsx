f0420f58661bd82ae25fe96f2f96333ceca9effe
Gaëtan <zodiaquestar@gmail.com>
2025-05-16 22:01:26 +0200

    feat(component): update ProductSpecs to include Stripe ID and adjust layout

--- src/components/ProductDetail/ProductSpecs.jsx
+++ src/components/ProductDetail/ProductSpecs.jsx
@@ -1,26 +1,29 @@
 import PropTypes from "prop-types";
+import AddToCartButton from "../ui/buttons/AddToCartButton";
 import { getPricingLabel } from "../utils/pricingLabel";
 
 const ProductSpecs = ({ product }) => {
   console.log(product);
   return (
-    <div className="mt-6 p-4 bg-gray-100 rounded-lg">
-      <h2 className="text-xl font-bold text-center text-primary mb-2">
-        Caractéristiques techniques
-      </h2>
-      <ul className="text-sm">
-        <li>
-          <strong>Modèle tarifaire :</strong>{" "}
-          {getPricingLabel(product.pricingModel)}
-        </li>
-        <li>
-          <strong>Marque :</strong> {product.brand}
-        </li>
-        <li>
-          <strong>ID Stripe :</strong> {product.priceId}
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="mt-6 p-4 bg-gray-100 rounded-lg">
+        <h2 className="text-xl font-bold text-center text-primary mb-2">
+          Caractéristiques techniques
+        </h2>
+        <ul className="text-sm">
+          <li>
+            <strong>Modèle tarifaire :</strong>{" "}
+            {getPricingLabel(product.pricingModel)}
+          </li>
+          <li>
+            <strong>Marque :</strong> {product.brand}
+          </li>
+        </ul>
+      </div>
+      <div className="w-full flex mt-2 p-2 items-center justify-center">
+        <AddToCartButton product={product} pricing={product.amount} />
+      </div>
+    </>
   );
 };
 
