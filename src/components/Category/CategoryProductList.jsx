a0c11fad8b03a0b34967248e5a998209c6d0a0d7
Gaëtan <zodiaquestar@gmail.com>
2025-05-17 12:54:26 +0200

    feat(category): ajouter la fonction de tri des produits par priorité

--- src/components/Category/CategoryProductList.jsx
+++ src/components/Category/CategoryProductList.jsx
@@ -1,27 +1,27 @@
 import PropTypes from "prop-types";
+import { sortProductsByPriority } from "../utils/sortProductByPriority";
 import ProductCard from "./ProductCard";
 
 const CategoryProductList = ({ element }) => {
-  // 1.State
-  // 2.Function
-  /* sorted by promo  */
-  // 3.Others
   const products = element.products || [];
-  // 4.Render
+  const sortedProducts = sortProductsByPriority(products);
+
   return (
     <main className="mt-6">
       <h2>Produits et Services</h2>
       <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((item) => {
-          return <ProductCard item={item} key={item.id} />;
-        })}
+        {sortedProducts.map((item) => (
+          <ProductCard item={item} key={item.id} />
+        ))}
       </section>
     </main>
   );
 };
 
 CategoryProductList.propTypes = {
-  element: PropTypes.object.isRequired,
+  element: PropTypes.shape({
+    products: PropTypes.array.isRequired,
+  }).isRequired,
 };
 
 export default CategoryProductList;
