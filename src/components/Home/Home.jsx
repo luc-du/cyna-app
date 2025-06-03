e1c647dc4d8566d0bf6e27156e71b397c29b42ce
Gaëtan <zodiaquestar@gmail.com>
2025-06-03 15:05:49 +0200

    SPRT0.05-REFACTO:homepage-deletion section promotion

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,4 @@
-import CategoriesGrid from "./CategoriesGrid";
 import HeroSection from "./HeroSection";
-import PromoSection from "./PromoSection";
-import TopProductsGrid from "./TopProductsGrid";
 
 const Home = () => {
   return (
@@ -12,10 +9,7 @@ const Home = () => {
       aria-label="Homepage main content"
     >
       <HeroSection />
-      {/* <section aria-label="Promotions">
-        <PromoSection />
-      </section>
-      <section aria-label="Categories">
+      {/* <section aria-label="Categories">
         <CategoriesGrid />
       </section>
       <section aria-label="Top Products">
