3c072d05d76fb56c426151957edda7d89d53af0f
Gaëtan <zodiaquestar@gmail.com>
2025-06-11 14:37:58 +0200

    feat(chore): subscriptionService WIP

---
+++ src/services/subscriptionService.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API_ROUTES } from "../api/apiRoutes";
+import { getToken } from "../components/utils/authStorage";
+
+export const subscriptionService = {
+  /* 
+  Récupérer les moyens de paiement de user
+  */
+};
+/* 
+Ajouter un moyen de paiement
+*/
+
+/* 
+Supprimer un moyen de paiement
+*/
+
+/* 
+Mettre à jour  un moyen de paiement
+*/
