e44e718463e42c9bef4e5c8a968082e79b651ff9
Gaëtan <zodiaquestar@gmail.com>
2025-06-17 19:56:40 +0200

    hofFixed(chore): auth register - fetUserProfile from userSlice != authSlice

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router";
 const PageNotFound = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
+    <div className="w-full flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800  dark:bg-gray-800">
       <h1 className="text-9xl font-extrabold text-purple-600 animate-bounce">
         404
       </h1>
       <h2 className="text-4xl font-semibold mt-4">Page Not Found</h2>
-      <p className="text-lg text-gray-600 mt-2">
+      <p className="text-lg text-gray-600 mt-2 dark:text-white">
         Oops! La page demandée n'existe pas.
       </p>
       <Link
